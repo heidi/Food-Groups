5fa076c896a2b81a186a4bf495306bf3ddb49dbe
Lilith Schneider <nekotsume@gmail.com>
2011-11-04 15:38:37 -0700

    initial project hello world

---
+++ FoodGroups/src/edu/berkeley/cs160/FoodGroupsActivity.java
@@ -0,0 +1,13 @@
+package edu.berkeley.cs160;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FoodGroupsActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
