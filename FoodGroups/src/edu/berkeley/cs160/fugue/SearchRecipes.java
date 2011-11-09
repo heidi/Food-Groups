191f7daf8e8989bccf6e16d8ef4b291434388861
unknown <KJ@KJ-PC.(none)>
2011-11-08 21:58:32 -0800

    search recipes.

--- FoodGroups/src/edu/berkeley/cs160/fugue/SearchRecipes.java
+++ FoodGroups/src/edu/berkeley/cs160/fugue/SearchRecipes.java
@@ -1,25 +1,69 @@
 package edu.berkeley.cs160.fugue;
 
 import android.app.Activity;
+import android.app.ListActivity;
+import android.app.SearchManager;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.RadioButton;
-import android.widget.TextView;
 import android.widget.Toast;
 import android.content.Intent;
 import android.widget.Button;
-
+import java.util.ArrayList;
 
 public class SearchRecipes extends Activity {
+	
+	private ListView aView;
+	private EditText typing;
+	private String lv_arr[]={"Android","Cupcake","Donut","Eclairs","AndroidPeople","Froyo",};
+	private ArrayList<String> arr_sort = new ArrayList<String>();
+	int textlength = 0;
 
 	/** Called when the activity is first created. */
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.searchrecipes);
+        
+        aView = (ListView) findViewById(R.id.listView1);
+        typing = (EditText) findViewById(R.id.editText1);
+        aView.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, lv_arr));
+        typing.addTextChangedListener(new TextWatcher(){
+        	
+        	public void afterTextChanged(Editable s) {
+        	}
+        	
+        	public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+        	}
+        	
+        	public void onTextChanged(CharSequence s, int start, int before, int count) {
+        		textlength=typing.getText().length();
+        		arr_sort.clear();
+        		for(int i=0;i<lv_arr.length;i++)
+        		{
+        			if(textlength<=lv_arr[i].length())
+        			{
+        				if(typing.getText().toString().equalsIgnoreCase((String) lv_arr[i].subSequence(0, textlength)))
+        				{
+        					arr_sort.add(lv_arr[i]);
+        				}
+        			}
+        		}
+        		
+        			aView.setAdapter(new ArrayAdapter<String>(SearchRecipes.this,android.R.layout.simple_list_item_1 , arr_sort));
+        		}
+        });
 
-        Button previous = (Button) findViewById(R.id.button1);
-               
+
+        
+        
+        
+
+        Button previous = (Button) findViewById(R.id.button1);               
         
         previous.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
