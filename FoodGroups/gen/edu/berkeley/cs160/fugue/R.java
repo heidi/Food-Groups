bbcca20095205f61cbe5f1594735f6c87bde9e3a
Lilith Schneider <nekotsume@gmail.com>
2011-11-18 17:56:02 -0800

    full leaderboards

--- FoodGroups/gen/edu/berkeley/cs160/fugue/R.java
+++ FoodGroups/gen/edu/berkeley/cs160/fugue/R.java
@@ -11,42 +11,53 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int baconwrappedasparagus=0x7f020000;
-        public static final int blankmeal=0x7f020001;
-        public static final int chilicheeseomelette=0x7f020002;
-        public static final int friendssearch=0x7f020003;
-        public static final int hamburgerandfries=0x7f020004;
-        public static final int icon=0x7f020005;
-        public static final int lemonbuttersalmon=0x7f020006;
-        public static final int pastasalad=0x7f020007;
-        public static final int tablecloth=0x7f020008;
-        public static final int tablewarejollyroger=0x7f020009;
+        public static final int albert=0x7f020000;
+        public static final int alice=0x7f020001;
+        public static final int baconwrappedasparagus=0x7f020002;
+        public static final int blankmeal=0x7f020003;
+        public static final int bob=0x7f020004;
+        public static final int chilicheeseomelette=0x7f020005;
+        public static final int chris=0x7f020006;
+        public static final int friendssearch=0x7f020007;
+        public static final int hamburgerandfries=0x7f020008;
+        public static final int heather=0x7f020009;
+        public static final int hidalgo=0x7f02000a;
+        public static final int icon=0x7f02000b;
+        public static final int jessica=0x7f02000c;
+        public static final int john=0x7f02000d;
+        public static final int lemonbuttersalmon=0x7f02000e;
+        public static final int matthew=0x7f02000f;
+        public static final int pastasalad=0x7f020010;
+        public static final int scorebox=0x7f020011;
+        public static final int tablecloth=0x7f020012;
+        public static final int tablewarejollyroger=0x7f020013;
     }
     public static final class id {
+        public static final int LinearLayout1=0x7f050026;
         public static final int addcooks_button=0x7f05000c;
-        public static final int addfriends_button=0x7f050032;
+        public static final int addfriends_button=0x7f050034;
         public static final int addpicture_button=0x7f05000a;
         public static final int alltimebutton=0x7f05001e;
         public static final int button1=0x7f050021;
         public static final int button2=0x7f050023;
         public static final int button3=0x7f050022;
         public static final int button4=0x7f050024;
-        public static final int datePicker1=0x7f050031;
-        public static final int detail=0x7f050028;
-        public static final int editText1=0x7f050033;
-        public static final int frameLayout1=0x7f050029;
-        public static final int frameLayout2=0x7f05002e;
+        public static final int datePicker1=0x7f050033;
+        public static final int detail=0x7f05002a;
+        public static final int editText1=0x7f050035;
+        public static final int frameLayout1=0x7f05002b;
+        public static final int frameLayout2=0x7f050030;
         public static final int friendlist=0x7f050005;
         public static final int friendsearch_textfield=0x7f050003;
         public static final int friendsearchback_button=0x7f050007;
         public static final int friendsearchdone_button=0x7f050008;
         public static final int friendsearchsearch_button=0x7f050004;
-        public static final int homecooking_button=0x7f05002c;
+        public static final int homecooking_button=0x7f05002e;
         public static final int homecookingback_button=0x7f05001a;
         public static final int homecookingdone_button=0x7f05001b;
         public static final int imageView1=0x7f050025;
-        public static final int img=0x7f050026;
-        public static final int leaderboard_button=0x7f05002a;
+        public static final int img=0x7f050027;
+        public static final int leaderboard_button=0x7f05002c;
         public static final int leaderboardback=0x7f050020;
         public static final int leaderboardlist=0x7f05001f;
         public static final int linearLayout1=0x7f050000;
@@ -56,14 +67,15 @@ public final class R {
         public static final int linearLayout5=0x7f050012;
         public static final int linearLayout6=0x7f050015;
         public static final int linearLayout7=0x7f050019;
-        public static final int listView1=0x7f050036;
-        public static final int menu_button=0x7f05002f;
-        public static final int planmealback_button=0x7f050034;
-        public static final int planmealdone_button=0x7f050035;
+        public static final int listView1=0x7f050038;
+        public static final int menu_button=0x7f050031;
+        public static final int planmealback_button=0x7f050036;
+        public static final int planmealdone_button=0x7f050037;
         public static final int recipe_textfeild=0x7f050018;
-        public static final int recipesearch_button=0x7f05002d;
+        public static final int recipesearch_button=0x7f05002f;
         public static final int recipetitle_textfield=0x7f05000d;
-        public static final int restaurant_button=0x7f05002b;
+        public static final int restaurant_button=0x7f05002d;
+        public static final int score=0x7f050028;
         public static final int scrollView1=0x7f050009;
         public static final int seekBar1=0x7f05000e;
         public static final int seekBar2=0x7f050011;
@@ -75,8 +87,8 @@ public final class R {
         public static final int textView4=0x7f050016;
         public static final int textView5=0x7f05000b;
         public static final int thisweekbutton=0x7f05001d;
-        public static final int timePicker1=0x7f050030;
-        public static final int title=0x7f050027;
+        public static final int timePicker1=0x7f050032;
+        public static final int title=0x7f050029;
         public static final int todaybutton=0x7f05001c;
     }
     public static final class layout {
--- FoodGroups/src/edu/berkeley/cs160/fugue/LeaderBoardActivity.java
+++ FoodGroups/src/edu/berkeley/cs160/fugue/LeaderBoardActivity.java
@@ -24,12 +24,31 @@ import android.widget.TextView;
 public class LeaderBoardActivity extends Activity implements OnClickListener {
 	
 	private LayoutInflater mInflater;
-	private Vector<Meal> meals;
+	private Vector<LBItem> meals;
+	private Vector<LBItem> weekList;
+	private Vector<LBItem> allTimeList;
+	private LBAdapter day;
+	private LBAdapter week;
+	private LBAdapter all;
+	private ListView leaderboard;
+	private Button today;
+	private Button thisWeek;
+	private Button allTime;
 	
-	String mealTitles[] = {"Veggie Pasta Salad - 86", "Lemon Butter Salmon - 69", "Bacon Wrapped Asparagus - 52", "Hamburger and Fries - 31", "Chili Cheese Omelette - 24"};
+	String mealTitles[] = {"Veggie Pasta Salad", "Lemon Butter Salmon", "Bacon Wrapped Asparagus", "Hamburger and Fries", "Chili Cheese Omelette"};
 	String mealParticipants[] = {"Alice, Harry, Melissa", "Albert", "John, Matthew", "Fred, George", "Jessica"};
 	Integer mealPics[] = {R.drawable.pastasalad, R.drawable.lemonbuttersalmon, R.drawable.baconwrappedasparagus, R.drawable.hamburgerandfries, R.drawable.chilicheeseomelette};
-
+	String mealScores[] = {"86", "69", "52", "31", "24"};
+	
+	String weekPeople[] = {"Jessica", "Albert", "Matthew", "Heather", "Chris"};
+	String weekScores[] = {"82.4", "75.7", "68.9", "57.2", "48.1"};
+	Integer weekPics[] = {R.drawable.jessica, R.drawable.albert, R.drawable.matthew, R.drawable.heather, R.drawable.chris};
+	
+	String topPeople[] = {"Alice", "John", "Chris", "Hidalgo", "Bob"};
+	String topDishes[] = {"Grilled Salmon and Asparagus", "Pork Roast with Kale Chips", "Spinach Salad", "Ahi Tuna with Salad", "Roast Beef with Potatoes and Veggies"};
+	Integer topPics[] = {R.drawable.alice, R.drawable.john, R.drawable.chris, R.drawable.hidalgo, R.drawable.bob};
+	String topScores[] = {"75.42", "72.39", "71.73", "68.96", "67.69"};
+	
     /** Called when the activity is first created. */
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,22 +57,33 @@ public class LeaderBoardActivity extends Activity implements OnClickListener {
         mInflater = (LayoutInflater) getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
 
         Button back = (Button) findViewById(R.id.leaderboardback);
-        Button today = (Button) findViewById(R.id.todaybutton);
-        Button thisWeek = (Button) findViewById(R.id.thisweekbutton);
-        Button allTime = (Button) findViewById(R.id.alltimebutton);
+        today = (Button) findViewById(R.id.todaybutton);
+        thisWeek = (Button) findViewById(R.id.thisweekbutton);
+        allTime = (Button) findViewById(R.id.alltimebutton);
+        
         back.setOnClickListener(this);
         today.setOnClickListener(this);
         thisWeek.setOnClickListener(this);
         allTime.setOnClickListener(this);
         
-        meals = new Vector<Meal>();
+        meals = new Vector<LBItem>();
+        weekList = new Vector<LBItem>();
+        allTimeList = new Vector<LBItem>();
         
         for (int i = 0; i < 5; i++) {
-        	meals.add(new Meal(i, mealPics[i], mealTitles[i], mealParticipants[i]));
+        	meals.add(new LBItem(i, mealPics[i], mealTitles[i], mealParticipants[i], mealScores[i]));
+        	weekList.add(new LBItem(i, weekPics[i], weekPeople[i], "", weekScores[i]));
+        	allTimeList.add(new LBItem(i, topPics[i], topPeople[i], "Top Dish: "+topDishes[i], topScores[i]));
         }
         
-        ListView leaderboard = (ListView) findViewById(R.id.leaderboardlist);
-        leaderboard.setAdapter(new MealAdapter(this, R.layout.leaderboarditem, R.id.title, meals));
+        day = new LBAdapter(this, R.layout.leaderboarditem, R.id.title, meals);
+        week = new LBAdapter(this, R.layout.leaderboarditem, R.id.title, weekList);
+        all = new LBAdapter(this, R.layout.leaderboarditem, R.id.title, allTimeList);
+        
+        leaderboard = (ListView) findViewById(R.id.leaderboardlist);
+        leaderboard.setAdapter(day);
+        
+        today.setTextSize(20);
         
 
     }
@@ -63,10 +93,22 @@ public class LeaderBoardActivity extends Activity implements OnClickListener {
 		switch (v.getId()) {
 		
 		case R.id.todaybutton:
+			leaderboard.setAdapter(day);
+			today.setTextSize(20);
+			thisWeek.setTextSize(14);
+			allTime.setTextSize(14);
 			break;
 		case R.id.thisweekbutton:
+			leaderboard.setAdapter(week);
+			today.setTextSize(14);
+			thisWeek.setTextSize(20);
+			allTime.setTextSize(14);
 			break;
 		case R.id.alltimebutton:
+			leaderboard.setAdapter(all);
+			today.setTextSize(14);
+			thisWeek.setTextSize(14);
+			allTime.setTextSize(20);
 			break;
 		case R.id.leaderboardback:
 			Intent intent = new Intent();
@@ -83,25 +125,28 @@ public class LeaderBoardActivity extends Activity implements OnClickListener {
 	// ********** //	Stole this code from web:
 	//						http://blog.sptechnolab.com/2011/02/01/android/android-custom-listview-items-and-adapters/
 	
-	class Meal {
+	class LBItem {
 		protected int mIndex;
 		protected int mId;
 		protected String mTitle;
 		protected String mDetail;
-		Meal(int index, int id,String title,String detail){
+		protected String mScore;
+		LBItem(int index, int id, String title, String detail, String score){
 			mIndex = index;
 			mId = id;
 			mTitle = title;
 			mDetail = detail;
+			mScore = score;
 		}
 		@Override
 		public String toString() {
-			return mId+" "+mTitle+" "+mDetail;
+			return mIndex+" "+mTitle+" "+mDetail+" "+mScore;
 		}
+		
 	}
 	
-	class MealAdapter extends ArrayAdapter<Meal> {
-		public MealAdapter(Context context, int resource, int textViewResourceId, List<Meal> objects) {               
+	class LBAdapter extends ArrayAdapter<LBItem> {
+		public LBAdapter(Context context, int resource, int textViewResourceId, List<LBItem> objects) {               
 			super(context, resource, textViewResourceId, objects);
 		}
 		@Override
@@ -110,19 +155,22 @@ public class LeaderBoardActivity extends Activity implements OnClickListener {
 			TextView title = null;
 			TextView detail = null;
 			ImageView image = null;
-			Meal meal = getItem(position);
+			TextView score = null;
+			LBItem it = getItem(position);
 			if (null == convertView) {
 				convertView = mInflater.inflate(R.layout.leaderboarditem, null);
 				holder = new ViewHolder(convertView);
 	            convertView.setTag(holder);
 	        }
 			holder = (ViewHolder) convertView.getTag();
-			title = holder.gettitle();
-			title.setText(meal.mTitle);
-			detail = holder.getdetail();
-			detail.setText(meal.mDetail);                                                     
+			title = holder.getTitle();
+			title.setText(it.mTitle);
+			detail = holder.getDetail();
+			detail.setText(it.mDetail);                                                     
 			image = holder.getImage();
-			image.setImageResource(meal.mId);
+			image.setImageResource(it.mId);
+			score = holder.getScore();
+			score.setText(it.mScore);
 			return convertView;
 		}
 	
@@ -130,27 +178,34 @@ public class LeaderBoardActivity extends Activity implements OnClickListener {
 			private View mRow;
 			private TextView title = null;
 			private TextView detail = null;
-			private ImageView image=null; 
+			private ImageView image = null; 
+			private TextView score = null;
 			public ViewHolder(View row) {
 				mRow = row;
 			}
-			public TextView gettitle() {
-				if(null == title){
+			public TextView getTitle() {
+				if (null == title) {
 					title = (TextView) mRow.findViewById(R.id.title);
 				}
 				return title;
 			}     
-			public TextView getdetail() {
-				if(null == detail){
+			public TextView getDetail() {
+				if (null == detail) {
 					detail = (TextView) mRow.findViewById(R.id.detail);
 				}
 				return detail;
 			}
 	        public ImageView getImage() {
-	        	if(null == image){
+	        	if (null == image) {
 	        		image = (ImageView) mRow.findViewById(R.id.img);
 	        	}
 	        	return image;
+	        }
+	        public TextView getScore() {
+	        	if (null == score) {
+	        		score = (TextView) mRow.findViewById(R.id.score);
+	        	}
+	        	return score;
 	        }
 		}
 	} 
